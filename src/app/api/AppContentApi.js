5e344b1d47b1d53dd16b0c22b8cbdb18576ba8f9
zain <zaininam699@gmail.com>
2023-06-03 02:15:38 +0500

    Commit Update

--- src/app/api/AppContentApi.js
+++ src/app/api/AppContentApi.js
@@ -3,10 +3,18 @@ import React, { useState, createContext } from 'react'
 const AppContext = createContext(null);
 
 const AppContentApi = ({ children }) => {
-
+    const [notification, setNotification] = useState(null);
+    const [location, setLocation] = useState(null);
+    const [storage, setStorage] = useState(null);
     const value = {
-
+        notification,
+        location,
+        storage,
+        setNotification,
+        setLocation,
+        setStorage,
     }
+    
     return (
         <AppContext.Provider value={value}>
             {children}
--- src/app/screens/Main/HomeScreen.js
+++ src/app/screens/Main/HomeScreen.js
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, {useEffect, useContext, useState} from 'react'
 import { SafeAreaView, View } from 'react-native'
 import { globalStyles } from '../../constants/Style'
 import Home from '../../components/Home/Home'
+import { 
+  requestForLocation,
+  requestForNotification,
+  requestForStorage,
+ } from '../../api/Permissions';
+import { AuthContext } from '../../api/AuthContentApi';
+import { AppContext } from '../../api/AppContentApi';
 const HomeScreen = () => {
+  const {isAppFirstLaunched} = useContext(AuthContext);
+  const {setNotification, setStorage, setLocation} = useContext(AppContext)
+  useEffect(() => {
+    const requestForPermissions = async () => {
+      if(isAppFirstLaunched){
+        setNotification(await requestForNotification());
+        setLocation(await requestForLocation());
+        setStorage(await requestForStorage());
+      }
+    }
+    requestForPermissions();
+
+  },[]);
+
+
+
+
   return (
     <SafeAreaView style={globalStyles.wrapper}>
         <View style={[globalStyles.wrapper, {paddingBottom:10}]}>
